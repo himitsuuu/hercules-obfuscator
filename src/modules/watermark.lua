83c9a44c7651d1d9157d6c30b27a370f3e8c3063
himitsuuu <92147743+himitsuuu@users.noreply.github.com>
2025-07-05 17:58:00 +0800

    Update watermark.lua

--- src/modules/watermark.lua
+++ src/modules/watermark.lua
@@ -2,7 +2,7 @@
 local Watermark = {}
 
 function Watermark.process(code)
-    return "--[Obfuscated by Hercules v1.6.2 | hercules-obfuscator.xyz/discord | hercules-obfuscator.xyz/source]\n" .. code
+    return "-- This file was protected by 'amariaim' v1.0.0 [discord.gg/amari]\n" .. code
 end
 
 return Watermark
